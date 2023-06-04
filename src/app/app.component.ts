22c3b26a6e3266a0722d26c9bb3d6c22eafb876c
laof <345603828@qq.com>
2023-06-04 12:57:58 +0800

    cicd:ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,8 @@ import { FooterComponent } from './footer/footer.component';
 })
 export class AppComponent {
   @ViewChild(FooterComponent) footerComponent!: FooterComponent;
-
   title = 'music';
-  constructor(private audio: AudioService) {}
-
-  test(r: string) {}
+  constructor() {}
 
   haha = false;
   fdfa(bb: boolean) {
